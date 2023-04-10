780144edda11961ae299d0b648e6190d9f62cdd7
Armando Jouhayerk <75504310+ArmandoJouhayerk@users.noreply.github.com>
2023-04-10 17:32:48 -0600

    Update clients.blade.php

--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -44,7 +44,6 @@
 
 <br><br>
  
-<a href="{{route('clients')}}"> Clients </a>
 <a href="{{ route('orderdetails') }}"> Orders Details </a> 
 <a href="{{ route('orders') }}"> Orders </a> 
 <a href="{{ route('products') }}"> Products </a>
