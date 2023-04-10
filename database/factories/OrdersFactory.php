f6482e4433f081fb3b108502eac50b2802711264
adriaanSM02 <126122414+adriaanSM02@users.noreply.github.com>
2023-04-10 16:33:09 -0600

    Update OrdersFactory.php

--- database/factories/OrdersFactory.php
+++ database/factories/OrdersFactory.php
@@ -9,14 +9,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Orders>
  */
 class OrdersFactory extends Factory
-=======
-use Illuminate\Support\Str;
-
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
-class OrdersFacttory extends Factory
->>>>>>> aa9685103967cb82859d3b9ad7f685a259400117
 {
     /**
      * Define the model's default state.
@@ -26,7 +18,6 @@ class OrdersFacttory extends Factory
     public function definition(): array
     {
         return [
-<<<<<<< HEAD
             'customer_id' => $this->faker->numberBetween(1,1000),
             'staff_id' => $this->faker->numberBetween(1,1000),
             'total' => $this->faker->randomFloat(4,50,100),
@@ -41,22 +32,4 @@ class OrdersFacttory extends Factory
 
         ];
     }
-=======
-            'customer_id' => 'factory:App\Customer',
-            'staff_id' => 'factory:App\Staff',
-            'total' => $this -> $faker -> randomFloat(),
-            'subtotal' => $this -> $faker -> randomFloat(),
-            'vat' => $this -> $faker -> randomFloat(),
-            'status' => $this -> $faker->randomElement(['in progress', 'sent', 'delivered']),
-            'notes' => $this -> $faker -> str_random(50),
-            'dateNtime' => $this -> $faker -> int_random(),
-            'photo1' => "",
-            'photo2' => "",
-            'active' => $this -> $faker->randomElement(['yes', 'no']),
-            
-        ]; 
-    }
-
-    
->>>>>>> aa9685103967cb82859d3b9ad7f685a259400117
 }
