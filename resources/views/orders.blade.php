3508112db86c608813ecb89e9f8352003b8446c6
Armando Jouhayerk <75504310+ArmandoJouhayerk@users.noreply.github.com>
2023-04-10 17:33:38 -0600

    Update orders.blade.php

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -51,7 +51,10 @@
 
 <br><br>
   
-  <a href="{{ route('orders') }}"> Orders </a> 
+<a href="{{route('clients')}}"> Clients </a>
+<a href="{{ route('orderdetails') }}"> Orders Details </a> 
+<a href="{{ route('products') }}"> Products </a>
+<a href="{{ route('staff') }}"> Staff </a> 
 
 </body>
 </html>
