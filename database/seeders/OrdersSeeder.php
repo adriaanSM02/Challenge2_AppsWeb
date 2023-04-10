68adc465121cf174a7e1da9b3351939a21dc3492
adriaanSM02 <126122414+adriaanSM02@users.noreply.github.com>
2023-04-10 16:34:35 -0600

    Update OrdersSeeder.php

--- database/seeders/OrdersSeeder.php
+++ database/seeders/OrdersSeeder.php
@@ -6,10 +6,6 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Orders;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> aa9685103967cb82859d3b9ad7f685a259400117
 class OrdersSeeder extends Seeder
 {
     /**
@@ -17,10 +13,6 @@ class OrdersSeeder extends Seeder
      */
     public function run(): void
     {
-<<<<<<< HEAD
-        //
-=======
-       
->>>>>>> aa9685103967cb82859d3b9ad7f685a259400117
+
     }
 }
